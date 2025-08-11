bdbdf41f93a40557e95f59e83b2af3f78e04678f
Karam Abbas <kma69362@gmail.com>
2025-08-11 15:22:55 -0700

    commit

---
+++ regex_array.js
@@ -0,0 +1,14 @@
+// Part 1: Filtering with regex
+
+const files = [
+  'document.txt',
+  'image.JPEG',
+  'photo.png',
+  'archive.zip',
+  'logo.gif',
+  'script.js',
+  'family-pic.jpg',
+];
+
+// Task: Create a function that takes an array of the filenames. Use the filter() array method. 
+// The filter condition should use a regular expression to identify image files. Return a new array containing only the image filenames.
