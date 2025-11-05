8d60ab2a2a2ada86419bc98b768c9365cae82818
Karam Abbas <kma69362@gmail.com>
2025-11-05 14:54:38 -0700

    commit

---
+++ Student.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const eventSchema = new mongoose.Schema({
+    firstName: {type: String, required: true, trim: true},
+    lastName: {type: String, required: true, trim: true},
+    email: {type: String, required: true, lowercase: true, unique: true},
+    cohort: {type: String, enum: ['Year 1', 'Year 2', 'Year 3'], default: 'Year 1'},
+    interests: {type: String, default: ""}
+    
+}, {timestamps: true})
