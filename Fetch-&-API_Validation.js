c674480a09ad5f50c37bbc03c8655cef69018d00
Karam Abbas <kma69362@gmail.com>
2026-01-08 14:13:40 -0700

    commit

--- Fetch-&-API_Validation.js
+++ Fetch-&-API_Validation.js
@@ -5,7 +5,7 @@ const getQuote = async () =>{
         if(!res.ok) throw new Error('Network response was not ok:' + res.statusText) // Error message if res isn't good.
 
         const data = await res.json()
-        const quote = data.quote
+        const quote = data.quote // This makes it so that it gets the quote itself and not the JSON itself
         console.log(quote)
 
     }catch(err){
