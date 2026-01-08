f7693655e4d1fd342506cfd6c80f626925dcd1f8
Karam Abbas <kma69362@gmail.com>
2026-01-08 14:10:27 -0700

    commit

--- Fetch-&-API_Validation.js
+++ Fetch-&-API_Validation.js
@@ -2,13 +2,14 @@ const getQuote = async () =>{
     try{
         const res = await fetch("https://api.kanye.rest/")
         
-        if(!res.ok) throw res // Throwing the res itself gives more context than just a simple message.
+        if(!res.ok) throw new Error('Network response was not ok:' + res.statusText) // Error message if res isn't good.
 
         const data = await res.json()
         const quote = data.quote
         console.log(quote)
+
     }catch(err){
-        console.error(err)
+        console.error("Looks like there was a problem.", err) // General error message.
     }
 }
 
